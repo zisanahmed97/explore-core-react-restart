0e4f5fc7f3e7abbdebb0b80c68eb248bed1e68c2
zisanahmed97 <zisanahmed97@gmail.com>
2026-07-28 00:06:12 +0600

    created first component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,42 @@ function App() {
 
   return (
     <>
-      
-
+      <Person></Person>
+      <Person></Person>
+      <Car></Car>
+      <Person></Person>
+       <Carlist></Carlist>
       
     </>
+  )
+}
+
+function Person(){
+  const name = "jolil";
+  const age= 23;
+  return(
+    <p>i am a person: {name} {age}</p>
+  )
+}
+
+function Car(){
+  return(
+    <div>
+      <h3>Audi</h3>
+      <p>this is my favorite car</p>
+    </div>
+  )
+}
+
+function Carlist(){
+  return(
+    <div>
+      <ul>
+        <li>Audi</li>
+        <li>bmw</li>
+        <li>mercedes</li>
+      </ul>
+    </div>
   )
 }
 
